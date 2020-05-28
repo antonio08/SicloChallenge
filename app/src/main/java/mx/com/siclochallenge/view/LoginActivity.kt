0057a266bd4be9f18a10fbb159e7975338797575
Antonio Lozano <alozano@omnitracs.com>
2020-05-28 13:01:52 -0500

    Feature: Create Login Activity + Login Activity created + Support for spanish + Roboto font added

---
+++ app/src/main/java/mx/com/siclochallenge/view/LoginActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Antonio Lozano on 5/27/2020.
+ */
+
+package mx.com.siclochallenge.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import mx.com.siclochallenge.R
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
